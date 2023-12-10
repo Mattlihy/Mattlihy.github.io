a0af32d0bb54feb079a20c92966a143c4f5f721c
Matt Li <86322124+Mattlihy@users.noreply.github.com>
2023-12-10 05:56:42 -0800

    2

--- script.js
+++ script.js
@@ -13,14 +13,34 @@ function submitRating() {
     ratings.push({ name, taste, ambiance, service, average });
 
     // Display ratings
-    document.getElementById("displayName").innerText = name;
-    document.getElementById("displayTaste").innerText = taste;
-    document.getElementById("displayAmbiance").innerText = ambiance;
-    document.getElementById("displayService").innerText = service;
-    document.getElementById("displayAverage").innerText = average.toFixed(2);
+    displayRatings();
 
-    // Show the result section
-    document.getElementById("result").classList.remove("hidden");
+    // Clear form fields
+    document.getElementById("name").value = "";
+    document.getElementById("taste").value = "";
+    document.getElementById("ambiance").value = "";
+    document.getElementById("service").value = "";
+}
+
+function displayRatings() {
+    const ratingsContainer = document.getElementById("ratingsContainer");
+    ratingsContainer.innerHTML = '';
+
+    // Display each set of ratings
+    ratings.forEach((rating, index) => {
+        const ratingBlock = document.createElement("div");
+        ratingBlock.classList.add("rating-block");
+
+        ratingBlock.innerHTML = `
+            <h2>${rating.name}</h2>
+            <p><strong>Taste:</strong> ${rating.taste}</p>
+            <p><strong>Ambiance:</strong> ${rating.ambiance}</p>
+            <p><strong>Service:</strong> ${rating.service}</p>
+            <p><strong>Overall Average:</strong> ${rating.average.toFixed(2)}</p>
+        `;
+
+        ratingsContainer.appendChild(ratingBlock);
+    });
 }
 
 function clearAllRatings() {
@@ -28,9 +48,5 @@ function clearAllRatings() {
     ratings = [];
 
     // Clear displayed ratings
-    document.getElementById("result").classList.add("hidden");
-    document.getElementById("name").value = "";
-    document.getElementById("taste").value = "";
-    document.getElementById("ambiance").value = "";
-    document.getElementById("service").value = "";
+    displayRatings();
 }
